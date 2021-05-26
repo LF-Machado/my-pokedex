595c8ef993f0023af95a886599c1be89bcdf6989
Luis Felipe Machado <luis.felipe.machado13@gmail.com>
2021-05-25 22:15:09 -0500

    Added Link from react-routerdom to nav-bar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return <div>
+    <Link to="/home">Home</Link>
+    <Link to="/favorites">Favorites</Link>
+  </div>;
+}
+
+export default NavBar;
