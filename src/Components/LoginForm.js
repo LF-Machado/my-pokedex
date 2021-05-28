25299e09e38e79bfc1d0e90d8c4a5e40eba5137d
Luis Felipe Machado <luis.felipe.machado13@gmail.com>
2021-05-28 14:16:58 -0500

    Added styles to LoginForm

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { Card, Form, Table, Button } from "react-bootstrap";
+import { Card, Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Visibility, VisibilityOff } from "@material-ui/icons";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [errors, setErrors] = useState({});
+  const [isValid, setIsValid] = useState(false);
   const logedIn = localStorage.getItem("logedIn");
   const history = useHistory();
 
@@ -50,21 +52,29 @@ function LoginForm() {
     }
 
     setErrors(errors);
+    setIsValid(formIsValid);
     return formIsValid;
   };
 
-  const loginFormStyle = {
-    display: "flex",
-    width: "100%",
-    height: "100%",
-  };
-
   return (
-    <div style={{ loginFormStyle }}>
-      <Card className="m-5 p-5">
+    <Card
+      style={{
+        width: "50%",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+      bg="primary"
+      border="dark"
+      text="light"
+    >
+      <Card.Header>Login</Card.Header>
+
+      <Card className=" m-0 pt-4 pr-5 pb-4 pl-5" bg="dark" text="light">
         <Form onSubmit={handleSubmit}>
-          <Table>
-            <tr>
+          <Container>
+            <Row>
               <Form.Label htmlFor="email">Email</Form.Label>
               <Form.Control
                 required
@@ -80,11 +90,23 @@ function LoginForm() {
               <Form.Control.Feedback type="invalid">
                 {errors.email}
               </Form.Control.Feedback>
-            </tr>
-            <tr>
-              <td>
-                <Form.Label htmlFor="password">Password</Form.Label>
+            </Row>
+            <Row className="mt-3 mb-0">
+              <Form.Label className="mb-2" htmlFor="password">
+                Password
+              </Form.Label>
+            </Row>
+            <Row className="mb-4 justify-content-md-between align-items-center ">
+              <Col
+                className="d-flex ml-0 p-0"
+                xs={12}
+                sm={12}
+                md={9}
+                lg={10}
+                xl={10}
+              >
                 <Form.Control
+                  className="mt-0"
                   required
                   placeholder="Password"
                   id="password"
@@ -98,21 +120,31 @@ function LoginForm() {
                 <Form.Control.Feedback type="invalid">
                   {errors.password}
                 </Form.Control.Feedback>
-              </td>
-              <td>
+              </Col>
+              <Col
+                className="d-flex ml-0 p-0 justify-content-end"
+                xs={12}
+                sm={2}
+                md={3}
+                lg={2}
+                xl={2}
+              >
                 <Button
+                  className="mt-0 ml-1"
                   type="button"
                   onClick={() => setShowPassword(!showPassword)}
                 >
-                  EYE
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
                 </Button>
-              </td>
-            </tr>
-            <Button variant="success">Submit</Button>
-          </Table>
+              </Col>
+            </Row>
+            <Button variant="success" type="submit">
+              Submit
+            </Button>
+          </Container>
         </Form>
       </Card>
-    </div>
+    </Card>
   );
 }
 
