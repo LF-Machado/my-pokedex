1a014327104a165b48bc0ba0a1426f6a5434abcc
Luis Felipe Machado <luis.felipe.machado13@gmail.com>
2021-05-28 20:48:14 -0500

    Changed background image for login

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -55,94 +55,113 @@ function LoginForm() {
   };
 
   return (
-    <Card
+    <div
       style={{
-        width: "50%",
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
+        // position: relative,
+        // text-align: center,
+        marginLeft: "auto",
+        marginRight: "auto",
+        minHeight: "100vh",
+        maxWidth: "100%",
+        backgroundImage: `url(
+          "https://i.pinimg.com/originals/18/9f/77/189f776f14db99a76061527c3afb5df8.jpg"
+        )`,
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+        backgroundColor: "black",
       }}
-      bg="primary"
-      border="dark"
-      text="light"
     >
-      <Card.Header>Login</Card.Header>
+      <Card
+        style={{
+          width: "50%",
+          position: "absolute",
+          top: "20%",
+          left: "10%",
+          transform: "translate(-10%, -20%)",
+        }}
+        bg="primary"
+        border="dark"
+        text="light"
+      >
+        <Card.Header>Login</Card.Header>
 
-      <Card className=" m-0 pt-4 pr-5 pb-4 pl-5" bg="dark" text="light">
-        <Form onSubmit={handleSubmit}>
-          <Container>
-            <Row>
-              <Form.Label htmlFor="email">Email</Form.Label>
-              <Form.Control
-                required
-                placeholder="Email"
-                id="email"
-                name="email"
-                type="text"
-                onChange={handleChange}
-                value={email}
-                isValid={!errors?.email}
-                isInvalid={errors?.email}
-              />
-              <Form.Control.Feedback type="invalid">
-                {errors.email}
-              </Form.Control.Feedback>
-            </Row>
-            <Row className="mt-3 mb-0">
-              <Form.Label className="mb-2" htmlFor="password">
-                Password
-              </Form.Label>
-            </Row>
-            <Row className="mb-4 justify-content-md-between align-items-start ">
-              <Col
-                className="d-flex ml-0 p-0 flex-column "
-                xs={12}
-                sm={12}
-                md={9}
-                lg={10}
-                xl={10}
-              >
+        <Card className=" m-0 pt-4 pr-5 pb-4 pl-5" bg="dark" text="light">
+          <Form onSubmit={handleSubmit}>
+            <Container>
+              <Row>
+                <Form.Label htmlFor="email">Email</Form.Label>
                 <Form.Control
-                  className="mt-0"
                   required
-                  placeholder="Password"
-                  id="password"
-                  name="password"
-                  type={showPassword ? "text" : "password"}
+                  placeholder="Email"
+                  id="email"
+                  name="email"
+                  type="text"
                   onChange={handleChange}
-                  value={password}
-                  isValid={!errors?.password}
-                  isInvalid={errors?.password}
+                  value={email}
+                  isValid={!errors?.email}
+                  isInvalid={errors?.email}
                 />
                 <Form.Control.Feedback type="invalid">
-                  {errors.password}
+                  {errors.email}
                 </Form.Control.Feedback>
-              </Col>
-              <Col
-                className="d-flex ml-0 p-0 justify-content-end"
-                xs={12}
-                sm={2}
-                md={3}
-                lg={2}
-                xl={2}
-              >
-                <Button
-                  className="mt-0 ml-1"
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
+              </Row>
+              <Row className="mt-3 mb-0">
+                <Form.Label className="mb-2" htmlFor="password">
+                  Password
+                </Form.Label>
+              </Row>
+              <Row className="mb-4 justify-content-md-between align-items-start ">
+                <Col
+                  className="d-flex ml-0 p-0 flex-column "
+                  xs={12}
+                  sm={12}
+                  md={9}
+                  lg={10}
+                  xl={10}
                 >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </Button>
-              </Col>
-            </Row>
-            <Button variant="success" type="submit">
-              Submit
-            </Button>
-          </Container>
-        </Form>
+                  <Form.Control
+                    className="mt-0"
+                    required
+                    placeholder="Password"
+                    id="password"
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    onChange={handleChange}
+                    value={password}
+                    isValid={!errors?.password}
+                    isInvalid={errors?.password}
+                  />
+                  <Form.Control.Feedback type="invalid">
+                    {errors.password}
+                  </Form.Control.Feedback>
+                </Col>
+                <Col
+                  className="d-flex ml-0 p-0 justify-content-end"
+                  xs={12}
+                  sm={2}
+                  md={3}
+                  lg={2}
+                  xl={2}
+                >
+                  <Button
+                    className="mt-0 ml-1"
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </Button>
+                </Col>
+              </Row>
+              <Button variant="success" type="submit">
+                Submit
+              </Button>
+            </Container>
+          </Form>
+        </Card>
       </Card>
-    </Card>
+    </div>
   );
 }
 
