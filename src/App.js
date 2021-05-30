9fc49b895e3524a6e7866344df47c8e78fde971f
Luis Felipe Machado <luis.felipe.machado13@gmail.com>
2021-05-30 11:32:07 -0500

    Added transition between pages

--- src/App.js
+++ src/App.js
@@ -10,18 +10,27 @@ import {
   Redirect,
 } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/detail" component={Detail} />
-          <Route exact path="/favorites" component={Favorites} />
-          <Redirect from="*" to="/" />
-        </Switch>
+        <Route
+          render={({ location }) => (
+            <TransitionGroup>
+              <CSSTransition key={location.key} timeout={300} classNames="fade">
+                <Switch location={location}>
+                  <Route exact path="/" component={Login} />
+                  <Route exact path="/home" component={Home} />
+                  <Route exact path="/detail" component={Detail} />
+                  <Route exact path="/favorites" component={Favorites} />
+                  <Redirect from="*" to="/" />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
       </Router>
     </div>
   );
